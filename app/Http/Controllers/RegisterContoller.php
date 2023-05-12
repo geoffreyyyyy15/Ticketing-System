45fc6085935d15950dc356127dfbeb2d73f81095
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-12 16:38:53 +0800

    Update RegisterContoller.php

--- app/Http/Controllers/RegisterContoller.php
+++ app/Http/Controllers/RegisterContoller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Livewire\RegisterForm;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterContoller extends Controller
@@ -9,5 +11,12 @@ class RegisterContoller extends Controller
     public function show()
     {
         return view('register.create');
+    }
+
+    public function store(RegisterForm $credentials)
+    {
+        $credentials['image'] = request()->file('image')->store('public');
+        $credentials['user_type'] = 1;
+        User::create($credentials);
     }
 }
