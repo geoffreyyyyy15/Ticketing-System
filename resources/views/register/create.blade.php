1c68d426737367371ed022c022e00f9a7d249fc9
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-12 16:42:11 +0800

    update register form margin

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class="flex items-center justify-center h-screen py-2 my-2">
-        <div class="flex justify-center md:mb-0 bg-gray-100 shadow-xl shadow-gray-900 w-[400px] p-2 m-2 rounded-lg">
+    <div class="flex items-center justify-center h-screen py-2 m-5 my-2">
+        <div class="flex justify-center mt-20 bg-gray-100 shadow-xl shadow-gray-900 w-[400px] p-2 m-2 rounded-lg">
                 <livewire:register-form />
         </div>
     </div>
