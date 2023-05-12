94ca1457d4bdf4b0d4c44b77d2f4eec373c753d2
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-12 16:40:39 +0800

    added sweetalert

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,19 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ticketing System</title>
+    <link rel="stylesheet" href="{{ asset('vendor/sweetalert2/sweetalert2.min.css') }}">
+    <script src="{{ asset('vendor/sweetalert2/sweetalert2.min.js') }}"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Poppins:wght@300;400&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
-
+    @include('sweetalert::alert')
     <livewire:styles />
 
-    
-</head>
-<body class="bg-gradient-to-r from-gray-500 to-white h-screen">
 
-    
+</head>
+<body class="h-screen bg-gradient-to-r from-gray-500 to-white">
+
+
     {{ $slot }}
 
     <livewire:scripts />
