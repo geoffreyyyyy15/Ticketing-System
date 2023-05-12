7be1cc58226c893673a0f7a85b18df17d03e5459
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-12 16:42:40 +0800

    update routes
    Grouping of routes middleware nad prefixes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterContoller;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::middleware(['auth'])->group(function () {
-
-});
+Route::prefix('user')->group(function() {
 Route::middleware(['guest'])->group( function () {
     // register routes
     Route::controller(RegisterContoller::class)->group(function () {
         Route::get('register' , 'show')->name('register');
+            Route::post('user', 'store')->name('user.store');
+        });
     });
+    Route::controller(LoginController::class)->group(function() {
+        Route::get('login', 'show')->name('login');
+    });
+});
+Route::prefix('user')->group(function() {
+    Route::middleware(['auth'])->group(function() {
+        Route::controller(LoginController::class)->group(function() {
+            Route::get('home', 'index')->name('home');
+            Route::get('logout', 'destroy')->name('logout');
+        });
+});
 });
