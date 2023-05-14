ca83fd9110bd03e4420fa2dda254d7bf67f090b0
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-05-14 20:25:20 +0800

    added loading logging in

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -9,9 +9,12 @@
     <a href="{{ route('register') }}">
         <h2 class="mb-2 text-center transition-all hover:scale-110 hover:text-gray-700">Sign Up here</h2>
         </a>
-
+        <div class="flex justify-center items-center">
+           <x-loading target="login" />
+    </div>
     <x-form.input name="email" wire:model.debounce.500ms='email' type="email" />
     <x-form.input name="password" wire:model.debounce.500ms='password' type="password" />
     <x-form.button >Login</x-form.button>
+  
 
 </form>
