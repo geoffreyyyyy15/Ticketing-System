7ff32b5a6f991b305447f4a54f26fd1992fa17ca
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-05-14 20:25:50 +0800

    Update index.blade.php

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,3 +1,4 @@
+
 <x-layout>
     <div class="flex items-center justify-center h-screen py-2 my-2">
         <div class="flex justify-center md:mb-0 bg-gray-100 shadow-xl shadow-gray-900 w-[400px] p-2 m-2 rounded-lg">
