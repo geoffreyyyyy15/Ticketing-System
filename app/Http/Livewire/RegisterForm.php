164f0abb5957f5a2d71da3934bbf56a7ae691c3b
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-05-14 22:40:27 +0800

    update image upload

--- app/Http/Livewire/RegisterForm.php
+++ app/Http/Livewire/RegisterForm.php
@@ -16,14 +16,16 @@ class RegisterForm extends Component
     public string $password = '';
     public string $name = '';
     public string $password_confirmation = '';
-    public $image = [];
+    public $image;
 
     protected $rules = [
         'email' => ['required', 'email', 'unique:users,email'],
         'username' => ['required', 'min:2', 'unique:users,username'],
         'name' => ['required', 'min:2'],
         'password' => ['required', 'min:8', 'confirmed'],
-        'image' => ['required', 'image','mimes:jpeg,png,jpg,gif,svg'],
+        'image' => ['required', 'array', 'min:1', 'max:3', 'mimes:jpeg,png,jpg,gif,svg', 'each' => 'image'],
+
+
     ];
     public function render()
     {
@@ -31,18 +33,28 @@ class RegisterForm extends Component
     }
     public function submit() {
         $credentials =  $this->validate();
-
-
-      // Register User
-      
-        $this->image->store('image');
+    
+        // Register User
+        if (is_array($this->image)) {
+            foreach ($this->image as $file) {
+                $path = $file->store('image');
+            }
+        } else {
+            $path = $this->image->store('image');
+        }
     
         $credentials['user_type'] = 1;
+        $credentials['image'] = $path;
         User::create($credentials);
-
+    
         return redirect()->route('login');
     }
+    
     public function login() {
         return redirect()->route('login');
+    }
+
+    public function updated($property) {
+        $this->validateOnly($property);
     }
 }
--- resources/views/livewire/image-upload.blade.php
+++ resources/views/livewire/image-upload.blade.php
@@ -1,18 +1,21 @@
-<x-form.field >
-    @if(!$image)
-    <x-loading target="image" />
-    @endif
-    @if($image)
-    <p class="p-2 m-2 text-lg text-gray-900">Preview</p>
-    <div class="flex items-center justify-center">        
-        @foreach ($image as $im)
-        <x-loading target="image" />
-        <img src="{{ $im->temporaryUrl() }}" alt="preview_image" class="w-[100px] h-[100px] rounded-full object-cover">
-        @endforeach
-    </div>
-    @endif
 
-    <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Profile Picture</label>
-<input wire:model='image' name="image" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-300 dark:placeholder-gray-400" id="file_input" type="file" multiple>
-<x-errors.input-error name="image" />
-</x-form.field>
+    <x-form.field >
+        @if(!$image)
+        <x-loading target="image" />
+        @endif
+        @if($image)
+        <p class="p-2 m-2 text-lg text-gray-900">Preview</p>
+        <div class="flex items-center justify-center">        
+            @foreach ($image as $im)
+            <x-loading target="image" />
+            <img src="{{ $im->temporaryUrl() }}" alt="preview_image" class="w-[100px] h-[100px] rounded-full object-cover">
+            @endforeach
+        </div>
+        @endif
+        <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Profile Picture</label>
+        <input wire:model='image' name="image" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-300 dark:placeholder-gray-400" id="file_input" type="file" multiple>
+        @error('image')
+        <p class="text-red-500 text-sm p-2 m-2">{{ $message }}</p>    
+        @enderror
+    </x-form.field>
+
