3cf4077fac0ed898dcf6255cd4e65eaeb1279cb7
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-05-14 20:23:31 +0800

    Update RegisterForm.php

--- app/Http/Livewire/RegisterForm.php
+++ app/Http/Livewire/RegisterForm.php
@@ -16,13 +16,14 @@ class RegisterForm extends Component
     public string $password = '';
     public string $name = '';
     public string $password_confirmation = '';
-    public $image;
+    public $image = [];
 
     protected $rules = [
         'email' => ['required', 'email', 'unique:users,email'],
         'username' => ['required', 'min:2', 'unique:users,username'],
         'name' => ['required', 'min:2'],
         'password' => ['required', 'min:8', 'confirmed'],
+        'image' => ['required', 'image','mimes:jpeg,png,jpg,gif,svg'],
     ];
     public function render()
     {
@@ -33,10 +34,15 @@ class RegisterForm extends Component
 
 
       // Register User
-      if ($this->image) {
-        $credentials['image'] = $this->image->store('image');
-    }
+      
+        $this->image->store('image');
+    
         $credentials['user_type'] = 1;
         User::create($credentials);
+
+        return redirect()->route('login');
+    }
+    public function login() {
+        return redirect()->route('login');
     }
 }
