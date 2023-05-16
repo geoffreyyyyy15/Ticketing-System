2f34612a8cf7cd4c88f5555927ba81f05429e7b2
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-16 16:28:21 +0800

    Update register-form.blade.php

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -5,7 +5,10 @@
         <h2 class="mb-2 text-center text-gray-400">Already have an account?</h2>
     </a>
     <hr class="mb-4 bg-gray-500">
-<x-form.input name="email" type="email" wire:model.debounce.500ms='email' />
+<x-form.input
+
+        name="email"
+        type="email" wire:model.debounce.500ms='email' />
 <x-form.input name="name"  wire:model.debounce.500ms='name' />
 <x-form.input name="username" wire:model.debounce.500ms='username' />
 <x-form.field >
@@ -14,7 +17,7 @@
     @endif
     @if($image)
     <p class="p-2 m-2 text-lg text-gray-900">Preview</p>
-    <div class="flex items-center justify-center">        
+    <div class="flex items-center justify-center">
         @foreach ($image as $im)
         <x-loading target="image" />
         <img src="{{ $im->temporaryUrl() }}" alt="preview_image" class="w-[100px] h-[100px] rounded-full object-cover">
@@ -24,7 +27,7 @@
     <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Profile Picture</label>
     <input wire:model='image' name="image" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-300 dark:placeholder-gray-400" id="file_input" type="file" multiple>
     @error('image')
-    <p class="text-red-500 text-sm p-2 m-2">{{ $message }}</p>    
+    <p class="text-red-500 text-sm p-2 m-2">{{ $message }}</p>
     @enderror
 </x-form.field>
 
