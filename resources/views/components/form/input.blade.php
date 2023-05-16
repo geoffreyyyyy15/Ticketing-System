c13789938e6ec3b1eeea4f51b15fc34a845d8a6c
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-16 16:27:44 +0800

    Update input.blade.php

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,7 +5,9 @@
 @endphp
 <x-form.field>
 <div class="relative z-0 w-full mb-6 group">
-    <input {{ $attributes->merge(['class' => $class]) }}
+    <input class="@error($name)
+    dark:border-red-500
+@enderror text-gray-900 block py-2.5 px-0 w-full text-sm  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" {{ $attributes->merge(['class' => $class]) }}
     type="{{ $type }}"
     name="{{ $name }}"
     id="floating_email"
