e75a0f535d75e9f2c2b97ae76b0e2595123efef2
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-16 16:27:32 +0800

    ticket seeder

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'title' => $this->faker->title(),
+            'description' => $this->faker->sentence(),
+            'priority' => $this->faker->randomElement([1, 2, 3])
+        ];
+    }
+}
