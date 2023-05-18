d8cd3fef5742f01c64f98b78097121b327afbd9e
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-18 16:56:23 +0800

    Update TicketController.php

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -9,6 +10,13 @@ class TicketController extends Controller
 {
     public function create() {
         alert()->success();
+
+        return back();
+    }
+    public function destroy(Request $request, Ticket $ticket)
+    {
+        $ticket->delete();
+        alert()->success('Success', 'Successfully Deleted');
 
         return back();
     }
