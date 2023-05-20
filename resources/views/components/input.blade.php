482bec116e79271390eff83ef653bed0eda46448
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-20 16:53:57 +0800

    update tickets table

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,4 +1,5 @@
+@props(['name', 'type' => 'text'])
 <div>
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+    <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> {{ ucwords($name) }}</label>
+    <input type="{{ $name }}" name="{{ $name }}" id="{{ $name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
 </div>
--- resources/views/livewire/tickets-table.blade.php
+++ resources/views/livewire/tickets-table.blade.php
@@ -75,6 +75,7 @@
                 <div class="px-6 py-6 lg:px-8">
                     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
                     <form class="space-y-6" action="#">
+                        <x-input name="title" />
                         <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                             Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
