c5e5374f93f237675ccae78b0ec35d571b931c2f
geoffreybagabaldo <geoffreyyyyy15@gmail.com>
2023-05-20 16:07:37 +0800

    Update DeleteTicket.php

--- app/Http/Livewire/DeleteTicket.php
+++ app/Http/Livewire/DeleteTicket.php
@@ -7,26 +7,40 @@ use Livewire\Component;
 
 class DeleteTicket extends Component
 {
-    public $ticketId;
+    public $ticket;
+    public $selectUserID = 0;
 
-    public function mount($ticketId)
+    public function mount($ticket)
     {
-        $this->ticketId = $ticketId;
+        $this->ticket = $ticket;
     }
 
     public function delete()
     {
-        Ticket::find($this->ticketId)->delete();
-
-        $this->dispatchBrowserEvent('refresh-page', ['timeout' => 2000]);
+        if($this->selectUserID == 0) {
+            return;
+        }
+        $ticket = Ticket::findOrfail($this->selectUserID);
+        $ticket->delete();
+        $this->selectUserID = 0;
+        // $this->dispatchBrowserEvent('refresh-page', ['timeout' => 2000]);
 
         // Perform the delete operation using the ticket ID
 
         // alert()->success('Success', 'You deleted the ticket successfully');
     }
 
+
     public function render()
     {
         return view('livewire.delete-ticket');
+    }
+
+    public function deleteTicket()
+    {
+        $this->delete();
+    }
+    public function changeDelete($ticket){
+        $this->selectUserID = $ticket;
     }
 }
