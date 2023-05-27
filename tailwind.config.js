043be085923b303ecfea8d0fa53a0965933295b1
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-05-27 23:58:21 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
+    // darkMode: 'class',
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
